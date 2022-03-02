2e5f9c9371e356176e93ded31f3bd2624d955a00
Q <rqjones@asu.edu>
2022-03-02 14:18:55 -0700

    Notes + added airdrop redeem and execute

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.8.10;
 // SPDX-License-Identifier: MIT
-
+import "./XRC20.sol";
 contract Aridrop{
+    //contract variables
     address private owner;
     uint public airdropCount=0;
     address XRC_Contract;
+    uint public TotalAlocated=0;
+    bool AirDropStatus;
     
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
     modifier isOwner() {
@@ -21,95 +24,56 @@ contract Aridrop{
         owner = msg.sender;
         emit OwnerSet(address(0), owner);
     }
-
+    //change owner
     function changeOwner(address newOwner) public isOwner {
         emit OwnerSet(owner, newOwner);
         owner = newOwner;
     }
+    //Set owner
     function getOwner() external view returns (address) {
         return owner;
     }
-
+    //Add User to contract
     function AddUser(address _User,uint _ammount)public isOwner returns(bool){
-        userAirdrop[airdropCount] = AirDropDB(_User,_ammount,true);
-        airdropCount++;
-        return true;
+        uint leftToBeAllocated = viewTokenInContract() - TotalAlocated;
+        if(leftToBeAllocated >0 && _ammount <= leftToBeAllocated){
+            userAirdrop[airdropCount] = AirDropDB(_User,_ammount,true);
+            airdropCount++;
+            TotalAlocated = TotalAlocated + _ammount;            
+            return true;
+        } else {
+            return false;
+        }
     }
+    //Edit Airdrop users
     function EditUser(uint _userCount,address _User,uint _ammount,bool _exist)public isOwner returns(bool){
         userAirdrop[_userCount] = AirDropDB(_User,_ammount,_exist);
         return true;
     }
+    //view accounts with pledged amounts
     function ViewUsers(uint _userCount) public view returns(address,uint,bool){
         return (userAirdrop[_userCount].User,userAirdrop[_userCount].ammount,userAirdrop[_userCount].exist);
     }
+    //Set contract
     function Set_XRC_Contract(address _Contract) public isOwner returns(address){
         XRC_Contract = _Contract;
         return XRC_Contract;
     }
-    function DeployAirDrop()public isOwner returns(bool){}
-
-}
-
-
-
-//------ test token contracts -------------------------------------------------------
-interface IERC20 {
-    function totalSupply() external view returns (uint);
-    function balanceOf(address account) external view returns (uint);
-    function transfer(address recipient, uint amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint);
-    function approve(address spender, uint amount) external returns (bool);
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-contract ERC20 is IERC20 {
-    uint public totalSupply=100;
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Solidity by Example";
-    string public symbol = "SOLBYEX";
-    uint8 public decimals = 18;
-
-    function transfer(address recipient, uint amount) external returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
+    //Deploy Airdrop
+    function DeployAirDrop(bool _status)public isOwner returns(bool){
+        AirDropStatus = _status;
+        return AirDropStatus;
     }
-
-    function approve(address spender, uint amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
+    //Users who were air dropped tokens can have them redeemed
+    function RedeemAirdrop(uint _countID)public returns(uint){
+        require(AirDropStatus == true);
+        //if(userAirdrop[_userCount].User == msg.sender){}
     }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool) {
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
-
-    function mint(uint amount) external {
-        balanceOf[msg.sender] += amount;
-        totalSupply += amount;
-        emit Transfer(address(0), msg.sender, amount);
-    }
-
-    function burn(uint amount) external {
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-        emit Transfer(msg.sender, address(0), amount);
+    //view Total totens to be airdropped
+    function viewTokenInContract()public view isOwner returns(uint){
+        uint tokensInContract = IERC20(XRC_Contract).balanceOf(address(this)); // this shows the balance of the XRC20 token in the Airdrop contract
+        return tokensInContract;
     }
 }
+
+
